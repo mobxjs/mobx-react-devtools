0d59c1debd77c078527e775ad4c040c5148e4ca2
Michel Weststrate <mweststrate@gmail.com>
2015-12-11 23:15:09 +0100

    Make sure all webpack artifacts are removed from the bundle, fixes #2

--- webpack.config.js
+++ webpack.config.js
@@ -17,5 +17,16 @@ module.exports = {
     externals: {
         "mobservable-react": "mobservable-react",
         "mobservable": "mobservable"
-    }
-}
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+            screw_ie8: true,
+            warnings: false
+            }
+        })
+    ]
+};
