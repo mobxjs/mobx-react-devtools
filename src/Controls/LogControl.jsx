c54fe6f9b713031628967de51e7ff000beeefffa
Peter Donald <peter@realityforge.org>
2017-09-10 21:17:05 +1000

    Remove unused import

--- src/Controls/LogControl.jsx
+++ src/Controls/LogControl.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { getGlobalState, setGlobalState, eventEmitter, restoreLogFromLocalstorage } from '../globalStore';
 
 export default class LogControl extends Component {
